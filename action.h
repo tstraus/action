b27cb3b0e3e629efcb255b5efd8048996a290f81
Tucker Strausbaugh <strausbaugh.tucker@gmail.com>
2018-06-18 20:44:57 -0400

    using lock_guards in all methods

--- action.h
+++ action.h
@@ -1,6 +1,7 @@
 #include <unordered_map>
 #include <functional>
 #include <utility>
+#include <mutex>
 
 namespace tstraus
 {
@@ -12,6 +13,8 @@ namespace tstraus
         template<typename Function>
         uint32_t operator += (Function&& f)
         {
+            std::lock_guard<std::mutex> lock(mtx);
+
             actions[nextID] = std::forward<Function>(f);
 
             return nextID++;
@@ -20,19 +23,25 @@ namespace tstraus
         // remove the function with the given id from the action
         void operator -= (uint32_t id)
         {
+            std::lock_guard<std::mutex> lock(mtx);
+
             actions.erase(id);
         }
 
         // execute the action
         template<class... Args>
-        void operator () (Args&&... args) const
+        void operator () (Args&&... args)
         {
+            std::lock_guard<std::mutex> lock(mtx);
+
             for(auto& f : actions)
                 f.second(args...);
         }
 
     private:
         uint32_t nextID;
+
+        std::mutex mtx;
 
         std::unordered_map<uint32_t, std::function<Signature>> actions;
     };
