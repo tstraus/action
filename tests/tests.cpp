d6720b65aef13bc5d5c880de743e71a1b9126aca
Tucker Strausbaugh <tckrsstrsbgh@gmail.com>
2018-06-21 19:55:45 -0400

    rewrite tests using lest, easier build on windows

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,110 +1,109 @@
 #include "action.h"
-#include "gtest/gtest.h"
+#include "lest.hpp"
 
 #include <iostream>
 
 using namespace std;
 using namespace tstraus;
 
-// setup the test fixture
-class ActionTests : public testing::Test
+const lest::test t[] =
 {
-protected:
-    virtual void SetUp()
+	// fixture for most tests
+    CASE("BasicTests" "[tstraus::Action]")
     {
-        x = 0;
+        SETUP("Set up an Action")
+        {
+            int x = 0;
+            Action<void()> action;
 
-        action += [&] {
-            x += 1;
-        };
-    }
+            action += [&] {
+                x += 1;
+            };
 
-    virtual void TearDown()
-    {
-        action.clear();
-    }
+			// make sure the function added in the fixture is called
+            SECTION("Add Member Operator")
+            {
+                action();
 
-    Action<void()> action;
+                EXPECT(x == 1);
+				EXPECT_NOT(x == 0);
+            }
 
-    int x;
+			// verify multiple functions are called
+            SECTION("Mutiple Functions")
+            {
+                action += [&] {
+                    x += 2;
+                };
+
+                action();
+
+                EXPECT(x == 3);
+				EXPECT_NOT(x == 1);
+            }
+
+			// remove a function when provided the id
+			SECTION("Remove Member Operator")
+			{
+				auto id = action += [&] {
+					x += 4;
+				};
+
+				action -= id;
+
+				action();
+
+				EXPECT(x == 1);
+				EXPECT_NOT(x == 5);
+			}
+
+			// set the action with only one function
+			SECTION("Make Only Operator")
+			{
+				action = [&] {
+					x += 4;
+				};
+
+				action();
+
+				EXPECT(x == 4);
+				EXPECT_NOT(x == 1);
+				EXPECT_NOT(x == 5);
+			}
+
+			// clear the action
+			SECTION("Clear")
+			{
+				action.clear();
+
+				action();
+
+				EXPECT(x == 0);
+				EXPECT_NOT(x == 1);
+			}
+        }
+	},
+
+	// be sure that arguments are passed through
+	CASE("String Action" "[tstraus::Action]")
+	{
+		string s = "";
+
+		Action<void(string)> action;
+
+		action += [&](string input) {
+			s = input;
+		};
+
+		action("asdf");
+
+		EXPECT(s == "asdf");
+		EXPECT_NOT(s == "");
+	}
 };
 
-// make sure the function added in the fixture is called
-TEST_F(ActionTests, AddMemberOperator)
+// run with '-p' to see each test
+int main(int argc, char** argv)
 {
-    action();
-
-    EXPECT_EQ(1, x);
-    EXPECT_NE(0, x);
-}
-
-// verify multiple functions are called
-TEST_F(ActionTests, MutipleFunctions)
-{
-    action += [&] {
-        x += 2;
-    };
-
-    action();
-
-    EXPECT_EQ(3, x);
-    EXPECT_NE(1, x);
-}
-
-// remove a function when provided the id
-TEST_F(ActionTests, RemoveMemberOperator)
-{
-    auto id = action += [&] {
-        x += 4;
-    };
-
-    action -= id;
-
-    action();
-
-    EXPECT_EQ(1, x);
-    EXPECT_NE(5, x);
-}
-
-// set the action with only one function
-TEST_F(ActionTests, MakeOnlyOperator)
-{
-    action = [&] {
-        x += 4;
-    };
-
-    action();
-
-    EXPECT_EQ(4, x);
-    EXPECT_NE(1, x);
-    EXPECT_NE(5, x);
-}
-
-// clear the action
-TEST_F(ActionTests, Clear)
-{
-    action.clear();
-
-    action();
-
-    EXPECT_EQ(0, x);
-    EXPECT_NE(1, x);
-}
-
-// non fixture tests
-// be sure the action gives arguments
-TEST(OtherActionTests, StringAction)
-{
-    string s = "";
-
-    Action<void(string)> action;
-
-    action += [&] (string input) {
-        s = input;
-    };
-
-    action("asdf");
-
-    EXPECT_EQ("asdf", s);
-    EXPECT_NE("", s);
+	return lest::run(t, argc, argv);
 }
